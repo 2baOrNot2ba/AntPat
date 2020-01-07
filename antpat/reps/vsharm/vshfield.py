8a2c349b0f28a5591fee043c95a818635cb4cd39
Tobia Carozzi <tobia@chalmers.se>
2020-01-07 11:01:32 +0100

    Cleanup PEP8

--- antpat/reps/vsharm/vshfield.py
+++ antpat/reps/vsharm/vshfield.py
@@ -1,18 +1,19 @@
 """Objects of this class are to be imported as Radiation Far-fields."""
 import numpy
+
+
 class vshField(object):
     def __init__(self, vshcoefs, frequencies=[0.0]):
-        self.frequencies=frequencies
-        self.vshcoefs=vshcoefs
-    
-    def getCoefAt(self,freq):
+        self.frequencies = frequencies
+        self.vshcoefs = vshcoefs
+
+    def getCoefAt(self, freq):
         return self.vshcoefs[self.getfreqidx(freq)]
-    
+
     def getfreqidx(self, freqval):
-        frequencies=numpy.array(self.frequencies)
-        freqidxlst=numpy.where(frequencies==freqval)
+        frequencies = numpy.array(self.frequencies)
+        freqidxlst = numpy.where(frequencies == freqval)
         if freqidxlst == []:
-           print("Frequency not found")
-           exit(1)
-        freqidx=freqidxlst[0][0] #For now assume unique value.
+            raise RuntimeError("Frequency not found")
+        freqidx = freqidxlst[0][0]  # For now assume unique value.
         return freqidx
