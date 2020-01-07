fd72d53fd8725e3fe06d7772a82e9fbdef0a4418
Tobia Carozzi <tobia@chalmers.se>
2020-01-07 11:08:16 +0100

    Implemented view of NEC file.

--- scripts/viewFFpat.py
+++ scripts/viewFFpat.py
@@ -9,23 +9,25 @@ from antpat.reps.sphgridfun import tvecfun
 from antpat.radfarfield import RadFarField
 from antpat.reps.vsharm.vshfield import vshField
 from antpat.reps.vsharm.coefs import load_SWE2vshCoef
-from antpat.io.filetypes import *
+import antpat.io.filetypes as ft
+from antpat.io.NECread import readNECout_tvecfuns
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("patternURL",
-                 help='Path to pattern file. Use format: filepath[#request]')
+                        help="""Path to pattern file.
+(Use format: filepath[#request])""")
     parser.add_argument("freq", nargs='?', type=float,
                         help="Frequency in Hertz")
     args = parser.parse_args()
     pattern_URL = urlparse(args.patternURL)
     FFfile = pattern_URL.path
     request = pattern_URL.fragment
-    if request == '': request = None
+    if request == '':
+        request = None
     freq = args.freq
-    if FFfile.endswith(FEKOsuffix):
-        #tvecfun.plotFEKO(FFfile, request, freq)
+    if FFfile.endswith(ft.FEKOsuffix):
         from antpat.reps.sphgridfun.tvecfun import TVecFields, plotvfonsph
         tvf = TVecFields()
         tvf.load_ffe(FFfile, request)
@@ -36,11 +38,14 @@ if __name__ == "__main__":
             frqIdx = int(numpy.interp(freq, freqs, range(len(freqs))))
         freq = freqs[frqIdx]
         print("Frequency={}".format(freq))
-        (THETA, PHI, E_th, E_ph) = (tvf.getthetas(), tvf.getphis(), tvf.getFthetas(freq), tvf.getFphis(freq))
+        (THETA, PHI, E_th, E_ph) = (tvf.getthetas(), tvf.getphis(),
+                                    tvf.getFthetas(freq), tvf.getFphis(freq))
         tvecfun.plotvfonsph(THETA, PHI, E_th, E_ph, freq=freq,
-                    vcoordlist=['Ludwig3','circ'], projection='azimuthal-equidistant',
-                    cmplx_rep='AbsAng', vfname=os.path.basename(FFfile))
-    elif FFfile.endswith(GRASPsuffix):
+                            vcoordlist=['Ludwig3', 'circ'],
+                            projection='azimuthal-equidistant',
+                            cmplx_rep='AbsAng',
+                            vfname=os.path.basename(FFfile))
+    elif FFfile.endswith(ft.GRASPsuffix):
         cfs, freq = load_SWE2vshCoef(FFfile, convention='FEKO')
         antFF = RadFarField(vshField([cfs], [freq]))
         THETA, PHI, V_th, V_ph = antFF.getFFongrid(freq)
@@ -50,11 +55,17 @@ if __name__ == "__main__":
         V2EfieldNrm = k*numpy.sqrt(Z0/(2*2*math.pi)) #Not sure about a 1/sqrt(2) factor
         E_th = V2EfieldNrm*V_th
         E_ph = V2EfieldNrm*V_ph
-        #plotAntPat2D(theta_var,phi_fix,E_th,E_ph)
         tvecfun.plotvfonsph(THETA, PHI, E_th, E_ph, freq,
-                            vcoord='sph',projection='equirectangular')
-    elif FFfile.endswith(NECsuffix):
-        print("Not implemented yet.")
+                            vcoord='sph', projection='equirectangular')
+    elif FFfile.endswith(ft.NECsuffix):
+        tvf = readNECout_tvecfuns(FFfile)
+        (THETA, PHI, E_th, E_ph) = (tvf.getthetas(), tvf.getphis(),
+                                    tvf.getFthetas(freq), tvf.getFphis(freq))
+        tvecfun.plotvfonsph(THETA, PHI, E_th, E_ph, freq=freq,
+                            vcoordlist=['Ludwig3'],
+                            projection='orthographic',
+                            cmplx_rep='AbsAng',
+                            vfname=os.path.basename(FFfile))
     else:
         print("Far-field pattern file type not known")
         exit(1)
