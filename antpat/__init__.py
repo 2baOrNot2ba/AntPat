39ca406cf1c2cc7741d5e216bb8a3a574e7cc21e
Tobia Carozzi <tobia@chalmers.se>
2023-04-18 17:00:26 +0200

    Added: physical EM constants. Incremented version.

--- antpat/__init__.py
+++ antpat/__init__.py
@@ -1,1 +1,8 @@
-__version__ = '0.4'
+__version__ = '0.5'
+
+# Some EM physical constants
+C_0 = 2.99792458e8  # m/s, Speed of light
+Z_0 = 376.730313668  # Ohms, Vacuum impedance
+MU_0 = 1.25663706212e-6  # N/A^2, Magnetic constant
+EPS_0 = 8.8541878128e-12  # F/m, Electric constant
+
